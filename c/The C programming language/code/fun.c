f618f20c5469bf8ff52ac42fbe913e11590ae6ef
xingxingso <1126973789@qq.com>
2019-04-17 11:42:34 +0800

    1

--- c/The C programming language/code/fun.c
+++ c/The C programming language/code/fun.c
@@ -498,5 +498,30 @@ int getline(char *line, int max)
     if (fgets(line, max, stdin) == NULL)
         return 0;
     else
-    return strlen(line);
+        return strlen(line);
+}
+
+// 6.3
+/* getword: get next word or character from input */
+int getword(char *word, int lim)
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+
+    while (isspace(c = getch()))
+        ;
+    if (c != EOF)
+        *w++ = c;
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++)
+        if (!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    *w = '\0';
+    return word[0];
 }
