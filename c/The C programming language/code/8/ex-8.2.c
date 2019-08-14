d5b6483c1c6df1efcf491d0df1187cdcf97e16d6
Kant Chan <1126973789@qq.com>
2019-08-14 13:40:41 +0800

    update

---
+++ c/The C programming language/code/8/ex-8.2.c
@@ -0,0 +1,27 @@
+// #include "syscalls.h" 
+#include <unistd.h>
+#include<stdio.h>
+
+int main() /* copy input to output */ 
+{ 
+    char buf[BUFSIZ]; 
+    int n; 
+
+    printf("%d\n", BUFSIZ);
+    while ((n = read(0, buf, BUFSIZ)) > 0) 
+        write(1, buf, n); 
+    return 0; 
+}
+
+/* getchar:  unbuffered single character input */ 
+int getchar1(void) 
+{ 
+    char c; 
+    return (read(0, &c, 1) == 1) ? (unsigned char) c : EOF; 
+} 
+
+/* getchar:  simple buffered version */ 
+int getchar(void) 
+{ 
+    static char buf[BUFSIZ]; 
+    static char *bufp = buf; static int n = 0; if (n == 0) {  /* buffer is empty */ n = read(0, buf, sizeof buf); bufp = buf; } return (--n >= 0) ? (unsigned char) *bufp++ : EOF; }
